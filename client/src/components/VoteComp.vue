2d309d45f37b0e5c3262731a010ac8560d42ca3a
Daniel Mensinger <daniel@mensinger-ka.de>
2023-10-28 13:49:07 +0200

    [FIX] Yarn lint fix

--- client/src/components/VoteComp.vue
+++ client/src/components/VoteComp.vue
@@ -18,13 +18,7 @@
           ></path>
         </g>
       </svg>
-      <svg
-        width="18"
-        height="18"
-        viewBox="0 0 1024 1024"
-        stroke-width="50"
-        v-if="realVote?.status == 'N'"
-      >
+      <svg width="18" height="18" viewBox="0 0 1024 1024" stroke-width="50" v-if="realVote?.status == 'N'">
         <g>
           <path
             d="M512.481 421.906L850.682 84.621c25.023-24.964 65.545-24.917 90.51.105s24.917 65.545-.105 90.51L603.03 512.377 940.94 850c25.003 24.984 25.017 65.507.033 90.51s-65.507 25.017-90.51.033L512.397 602.764 174.215 940.03c-25.023 24.964-65.545 24.917-90.51-.105s-24.917-65.545.105-90.51l338.038-337.122L84.14 174.872c-25.003-24.984-25.017-65.507-.033-90.51s65.507-25.017 90.51-.033L512.48 421.906z"
@@ -156,7 +150,7 @@ export default defineComponent({
 }
 
 .ballot-box-active {
-  --bs-bg-opacity: .75 !important;
+  --bs-bg-opacity: 0.75 !important;
   cursor: pointer;
 
   border-color: black;
